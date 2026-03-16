993bb9db594212d1038004a56e4ab11c7f44d657
Shruti <shruti.bhadane@enfec.com>
2026-03-16 14:36:39 +0530

    Mutual Funds & Buy Stocks Screen Graph design

--- app/src/main/java/com/assignments/stockmarket/graph/CandleStickChart.kt
+++ app/src/main/java/com/assignments/stockmarket/graph/CandleStickChart.kt
@@ -11,132 +11,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Canvas
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.times
-import java.nio.file.Files.size
+import com.assignments.stockmarket.R
 import kotlin.math.abs
 import kotlin.math.min
-
-/*
-@Composable
-fun CandleStickChart(
-    candles: List<Candle>,
-    modifier: Modifier = Modifier
-) {
-
-    val green = Color(0xFF7CFC00)
-    val red = Color(0xFFFF6B6B)
-
-    Canvas(modifier = modifier.background(Color(0xFF001B3A))) {
-
-        val spacePerCandle = size.width / candles.size
-        val candleWidth = size.width / (candles.size * 0.25f)
-
-        val maxPrice = candles.maxOf { it.high }
-        val minPrice = candles.minOf { it.low }
-
-        val priceRange = maxPrice - minPrice
-
-        fun priceToY(price: Float): Float {
-            return size.height - ((price - minPrice) / priceRange) * size.height
-        }
-
-        candles.forEachIndexed { index, candle ->
-
-            val x = index * candleWidth * spacePerCandle / 2
-
-            val openY = priceToY(candle.open)
-            val closeY = priceToY(candle.close)
-            val highY = priceToY(candle.high)
-            val lowY = priceToY(candle.low)
-
-            val color = if (candle.close >= candle.open) green else red
-
-            // Draw wick
-            drawLine(
-                color = color,
-                start = Offset(x, highY),
-                end = Offset(x, lowY),
-                strokeWidth = 2f
-            )
-
-            // Draw body
-            drawRect(
-                color = color,
-                topLeft = Offset(x - candleWidth / 2, min(openY, closeY)),
-                size = Size(
-                    candleWidth,
-                    abs(openY - closeY)
-                )
-            )
-        }
-    }
-}*/
-
-
-
-/*
-@Composable
-fun CandleStickChart(
-    candles: List<Candle>,
-    modifier: Modifier = Modifier
-) {
-
-    val green = Color(0xFF7CFC00)
-    val red = Color(0xFFFF6B6B)
-
-    Canvas(
-        modifier = modifier.background(Color(0xFF001B3A))
-    ) {
-
-        val spacePerCandle = size.width / candles.size
-        val candleWidth = spacePerCandle * 0.65f   // thinner candles
-
-        val maxPrice = candles.maxOf { it.high }
-        val minPrice = candles.minOf { it.low }
-        val priceRange = maxPrice - minPrice
-
-        fun priceToY(price: Float): Float {
-            return size.height - ((price - minPrice) / priceRange) * size.height
-        }
-
-        candles.forEachIndexed { index, candle ->
-
-            val centerX = index * spacePerCandle + spacePerCandle / 2
-
-            val openY = priceToY(candle.open)
-            val closeY = priceToY(candle.close)
-            val highY = priceToY(candle.high)
-            val lowY = priceToY(candle.low)
-
-            val color = if (candle.close >= candle.open) green else red
-
-            // Thin wick
-            drawLine(
-                color = color,
-                start = Offset(centerX, highY),
-                end = Offset(centerX, lowY),
-                strokeWidth = 2f
-            )
-
-            // Thin body
-            drawRect(
-                color = color,
-                topLeft = Offset(
-                    centerX - candleWidth / 2,
-                    min(openY, closeY)
-                ),
-                size = Size(
-                    candleWidth,
-                    abs(openY - closeY)
-                )
-            )
-        }
-    }
-}*/
-
 
 @Composable
 fun ScrollableCandleChart(
@@ -160,7 +41,7 @@ fun ScrollableCandleChart(
             modifier = Modifier
                 .width(chartWidth)
                 .height(300.dp)
-                .background(Color(0xFF001B3A))
+                .background(colorResource(R.color.screen_background))
         ) {
 
             val candleWidthPx = candleWidth.toPx()
